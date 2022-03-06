8fee9d86edfda15d57cbe74c7cd59c2ccbc2594a
tsurumoto <tsurumotokentarou@gmail.com>
2022-03-06 23:56:36 +0900

    to public

--- Sources/MusicPlayer/MPConstants.swift
+++ Sources/MusicPlayer/MPConstants.swift
@@ -10,63 +10,63 @@ import Foundation
 public final class MPConstants {
     
     public struct Pitch {
-        static var minValue: Float {
+        public static var minValue: Float {
             return -800
         }
         
-        static var maxValue: Float {
+        public static var maxValue: Float {
             return 800
         }
         
         /// デバイスの限界値
-        static var limitMinValue: Float {
+        public static var limitMinValue: Float {
             return -2400
         }
         
         /// デバイスの限界値
-        static var limitMaxValue: Float {
+        public static var limitMaxValue: Float {
             return 2400
         }
         
-        static var unit: Float {
+        public static var unit: Float {
             return 100
         }
         
-        static let defaultValue: Float = 0.0
+        public static let defaultValue: Float = 0.0
         
-        static let plusMark: String = "#"
+        public static let plusMark: String = "#"
         
-        static let minusMark: String = "♭"
+        public static let minusMark: String = "♭"
     }
 
     public struct Rate {
-        static var minValue: Float {
+        public static var minValue: Float {
             return 0.25
         }
         
-        static var maxValue: Float {
+        public static var maxValue: Float {
             return 4.0
         }
         
         /// デバイスの限界値
-        static var limitMinValue: Float {
+        public static var limitMinValue: Float {
             return 0.25
         }
         
         /// デバイスの限界値
-        static var limitMaxValue: Float {
+        public static var limitMaxValue: Float {
             return 4.0
         }
         
-        static var unit: Float {
+        public static var unit: Float {
             return 0.25
         }
         
-        static var defaultValue: Float = 1.0
+        public static var defaultValue: Float = 1.0
         
-        static let plusMark: String = "+"
+        public static let plusMark: String = "+"
         
-        static let minusMark: String = "-"
+        public static let minusMark: String = "-"
     }
 }
 
