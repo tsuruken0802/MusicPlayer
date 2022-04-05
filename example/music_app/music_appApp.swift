fb76336dccbe13e00fbd6deff092798888dc705c
tsurumoto <tsurumotokentarou@gmail.com>
2022-04-05 21:57:18 +0900

    add init audioSession to example

--- example/music_app/music_appApp.swift
+++ example/music_app/music_appApp.swift
@@ -24,6 +24,21 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         MPMediaLibrary.requestAuthorization { _ in
         }
         UIApplication.shared.beginReceivingRemoteControlEvents()
+        setAudioSession()
         return true
+    }
+}
+
+private extension AppDelegate {
+    /// set audio session
+    func setAudioSession() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback, mode: .default)
+            try session.setActive(true)
+        }
+        catch let e {
+            print(e.localizedDescription)
+        }
     }
 }
