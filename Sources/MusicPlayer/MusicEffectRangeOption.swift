5cdf5199ecfe8f04e2873626273892d45eca2a2f
tsurumoto <tsurumotokentarou@gmail.com>
2022-06-25 22:10:42 +0900

    to public

--- Sources/MusicPlayer/MusicEffectRangeOption.swift
+++ Sources/MusicPlayer/MusicEffectRangeOption.swift
@@ -12,4 +12,14 @@ public struct MusicEffectRangeOption {
     public let maxValue: Float
     public let unit: Float
     public let defaultValue: Float
+    
+    public init(minValue: Float,
+                maxValue: Float,
+                unit: Float,
+                defaultValue: Float) {
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.unit = unit
+        self.defaultValue = defaultValue
+    }
 }
